50593e61fb3ab6d238cac74bce26a8dea441c366
李巨丰 <920347627@qq.com>
2020-09-20 12:56:51 +0800

    tidb format功能

--- flink-formats/flink-tidb-pb/src/main/java/org/apache/flink/tidb/pb/TidbProtoBufFormatFactory.java
+++ flink-formats/flink-tidb-pb/src/main/java/org/apache/flink/tidb/pb/TidbProtoBufFormatFactory.java
@@ -13,6 +13,9 @@ import org.apache.flink.table.factories.DynamicTableFactory;
 import org.apache.flink.table.factories.FactoryUtil;
 import org.apache.flink.table.factories.SerializationFormatFactory;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,6 +23,21 @@ import java.util.Set;
 public class TidbProtoBufFormatFactory implements DeserializationFormatFactory, SerializationFormatFactory {
 
 	public static final String IDENTIFIER = "tidb-pb";
+	/** Formatter for SQL string representation of a time value. */
+	static final DateTimeFormatter SQL_TIME_FORMAT = new DateTimeFormatterBuilder()
+		.appendPattern("HH:mm:ss")
+		.appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+		.toFormatter();
+
+	/** Formatter for SQL string representation of a timestamp value (without UTC timezone). */
+	static final DateTimeFormatter SQL_TIMESTAMP_FORMAT = new DateTimeFormatterBuilder()
+		.append(DateTimeFormatter.ISO_LOCAL_DATE)
+		.appendLiteral(' ')
+		.append(SQL_TIME_FORMAT)
+		.toFormatter();
+	/** Formatter for ISO8601 string representation of a timestamp value (without UTC timezone). */
+	static final DateTimeFormatter ISO8601_TIMESTAMP_FORMAT = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
 	public static final ConfigOption<Boolean> IGNORE_PARSE_ERRORS = ConfigOptions
 		.key("ignore-parse-errors")
 		.booleanType()
--- flink-formats/flink-tidb-pb/src/main/java/org/apache/flink/tidb/pb/TidbProtobufDeserializationSchema.java
+++ flink-formats/flink-tidb-pb/src/main/java/org/apache/flink/tidb/pb/TidbProtobufDeserializationSchema.java
@@ -1,15 +1,10 @@
 package org.apache.flink.tidb.pb;
 
-import com.googlecode.protobuf.format.JsonFormat;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.flink.table.data.ArrayData;
-import org.apache.flink.table.data.GenericRowData;
-import org.apache.flink.table.data.RowData;
-import org.apache.flink.table.data.StringData;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.table.api.TableException;
+import org.apache.flink.table.data.*;
 import org.apache.flink.table.types.logical.*;
 import org.apache.flink.types.RowKind;
 import org.apache.flink.util.Collector;
@@ -17,69 +12,40 @@ import org.apache.flink.tidb.pb.BinlogMessage.*;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalQueries;
 import java.util.List;
 
+
 import static java.lang.String.format;
-import static org.apache.flink.util.Preconditions.checkNotNull;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static org.apache.flink.tidb.pb.TidbProtoBufFormatFactory.*;
 
 public class TidbProtobufDeserializationSchema implements DeserializationSchema<RowData> {
 	private static final String OP_INSERT = "Insert";
 	private static final String OP_UPDATE = "Update";
 	private static final String OP_DELETE = "Delete";
-
-	private RowType rowType;
+	private DeserializationRuntimeConverter converter;
 	private TypeInformation<RowData> resultTypeInfo;
 	private boolean ignoreParseErrors;
 
 	/**
-	 * Runtime converter that converts {@link JsonNode}s into
-	 * objects of Flink SQL internal data structures. **/
-	private final DeserializationRuntimeConverter runtimeConverter;
+	 * Timestamp format specification which is used to parse timestamp.
+	 */
+	private final TimestampFormat timestampFormat;
 
 	public TidbProtobufDeserializationSchema(RowType rowType, TypeInformation<RowData> resultTypeInfo, boolean ignoreParseErrors) {
-		this.rowType = rowType;
 		this.resultTypeInfo = resultTypeInfo;
 		this.ignoreParseErrors = ignoreParseErrors;
-		this.runtimeConverter = createRowConverter(checkNotNull(rowType));
-
+		this.converter = createRowConverter(rowType);
+		this.timestampFormat = null;
 	}
 
-	private DeserializationRuntimeConverter createRowConverter(RowType rowType) {
-		final DeserializationRuntimeConverter[] fieldConverters = rowType.getFields().stream()
-			.map(RowType.RowField::getType)
-			.map(this::createConverter)
-			.toArray(DeserializationRuntimeConverter[]::new);
-		final String[] fieldNames = rowType.getFieldNames().toArray(new String[0]);
 
-		return jsonNode -> {
-			ObjectNode node = (ObjectNode) jsonNode;
-			int arity = fieldNames.length;
-			GenericRowData row = new GenericRowData(arity);
-			for (int i = 0; i < arity; i++) {
-				String fieldName = fieldNames[i];
-				JsonNode field = node.get(fieldName);
-				Object convertedField = convertField(fieldConverters[i], fieldName, field);
-				row.setField(i, convertedField);
-			}
-			return row;
-		};
-	}
-
-	private Object convertField(
-		DeserializationRuntimeConverter fieldConverter,
-		String fieldName,
-		JsonNode field) {
-		if (field == null) {
-			if (failOnMissingField) {
-				throw new JsonParseException(
-					"Could not find field with name '" + fieldName + "'.");
-			} else {
-				return null;
-			}
-		} else {
-			return fieldConverter.convert(field);
-		}
-	}
 	@Override
 	public RowData deserialize(byte[] message) throws IOException {
 		throw new RuntimeException(
@@ -88,74 +54,61 @@ public class TidbProtobufDeserializationSchema implements DeserializationSchema<
 
 	@Override
 	public void deserialize(byte[] message, Collector<RowData> out) throws IOException {
-		int fieldCount = rowType.getFieldCount();
-		Binlog binlog = Binlog.parseFrom(message);
-		ObjectMapper mapper = new ObjectMapper();
-		String jsonBinlog = JsonFormat.printToString(binlog);
-		JsonNode jsonNode = mapper.readTree(jsonBinlog);
-
-		// only process dml sql
-		if (binlog.hasDmlData()) {
-			DMLData dmlData = binlog.getDmlData();
-			List<Table> tablesList = dmlData.getTablesList();
-			for (int i = 0; i < tablesList.size(); i++) {
-				Table table = tablesList.get(i);
-				List<ColumnInfo> columnInfoList = table.getColumnInfoList();
-				// one table may contains multiple mutations
-				List<TableMutation> mutationsList = table.getMutationsList();
-				for (int j = 0; j < mutationsList.size(); j++) {
-					TableMutation tableMutation = mutationsList.get(j);
-					String opName = tableMutation.getType().name();
-					if (OP_INSERT.equals(opName)) {
-						List<Column> columnsList = tableMutation.getRow().getColumnsList();
-						GenericRowData insert = new GenericRowData(RowKind.INSERT, fieldCount);
-						for (int k = 0; k < columnsList.size(); k++) {
-							ColumnInfo columnInfo = columnInfoList.get(k);
-							Column column = columnsList.get(k);
-							Object value = getColumnValueString(column, columnInfo);
-							insert.setField(k, value);
+		try {
+			Binlog binlog = Binlog.parseFrom(message);
+			// only process dml sql
+			if (binlog.hasDmlData()) {
+				DMLData dmlData = binlog.getDmlData();
+				List<Table> tablesList = dmlData.getTablesList();
+				for (int i = 0; i < tablesList.size(); i++) {
+					Table table = tablesList.get(i);
+					List<ColumnInfo> columnInfoList = table.getColumnInfoList();
+					// one table may contains multiple mutations
+					List<TableMutation> mutationsList = table.getMutationsList();
+					for (int j = 0; j < mutationsList.size(); j++) {
+						TableMutation tableMutation = mutationsList.get(j);
+						String opName = tableMutation.getType().name();
+						if (OP_INSERT.equals(opName)) {
+							List<Column> columnsList = tableMutation.getRow().getColumnsList();
+							Tuple2<List<ColumnInfo>, List<Column>> columnsInfoAndValuesTuple = new Tuple2<>(columnInfoList, columnsList);
+							RowData insert = (RowData) converter.convert(columnsInfoAndValuesTuple);
+							insert.setRowKind(RowKind.INSERT);
 							out.collect(insert);
-						}
-					} else if (OP_UPDATE.equals(opName)) {
-						List<Column> columnsList = tableMutation.getRow().getColumnsList();
-						List<Column> changeColumnsList = tableMutation.getChangeRow().getColumnsList();
+						} else if (OP_UPDATE.equals(opName)) {
+							List<Column> columnsList = tableMutation.getRow().getColumnsList();
+							List<Column> changeColumnsList = tableMutation.getChangeRow().getColumnsList();
 
-						GenericRowData before = new GenericRowData(RowKind.UPDATE_BEFORE, fieldCount);
-						GenericRowData after = new GenericRowData(RowKind.UPDATE_AFTER, fieldCount);
+							Tuple2<List<ColumnInfo>, List<Column>> columnsInfoAndValuesTuple = new Tuple2<>(columnInfoList, columnsList);
+							Tuple2<List<ColumnInfo>, List<Column>> changeColumnsInfoAndValuesTuple = new Tuple2<>(columnInfoList, changeColumnsList);
 
-						for (int k = 0; k < columnsList.size(); k++) {
-							ColumnInfo columnInfo = columnInfoList.get(k);
+							RowData before = (RowData) converter.convert(changeColumnsInfoAndValuesTuple);
+							RowData after = (RowData) converter.convert(columnsInfoAndValuesTuple);
 
-							Column column = columnsList.get(k);
-							Column changeColumn = changeColumnsList.get(k);
-
-							Object value = getColumnValueString(column, columnInfo);
-							Object changeValue = getColumnValueString(changeColumn, columnInfo);
-
-							before.setField(k, value);
-							after.setField(k, changeValue);
+							before.setRowKind(RowKind.UPDATE_BEFORE);
+							after.setRowKind(RowKind.UPDATE_AFTER);
 
 							out.collect(before);
 							out.collect(after);
-						}
-					} else if (OP_DELETE.equals(opName)) {
-
-						List<Column> columnsList = tableMutation.getRow().getColumnsList();
-						GenericRowData delete = new GenericRowData(RowKind.DELETE, fieldCount);
-						for (int k = 0; k < columnsList.size(); k++) {
-							ColumnInfo columnInfo = columnInfoList.get(k);
-							Column column = columnsList.get(k);
-							Object value = getColumnValueString(column, columnInfo);
-							delete.setField(k, value);
+						} else if (OP_DELETE.equals(opName)) {
+							List<Column> columnsList = tableMutation.getRow().getColumnsList();
+							Tuple2<List<ColumnInfo>, List<Column>> columnsInfoAndValuesTuple = new Tuple2<>(columnInfoList, columnsList);
+							RowData delete = (RowData) converter.convert(columnsInfoAndValuesTuple);
+							delete.setRowKind(RowKind.DELETE);
 							out.collect(delete);
-						}
-					} else {
-						if (!ignoreParseErrors) {
-							throw new IOException(format(
-								"Unknown \"type\" value \"%s\". The Tidb protobuf message is '%s'", opName, new String(message)));
+						} else {
+							if (!ignoreParseErrors) {
+								throw new IOException(format(
+									"Unknown \"type\" value \"%s\". The Tidb protobuf message is '%s'", opName, new String(message)));
+							}
 						}
 					}
 				}
+			}
+		} catch (Throwable t) {
+			// a big try catch to protect the processing.
+			if (!ignoreParseErrors) {
+				throw new IOException(format(
+					"Corrupt TiDb protobuf message '%s'.", new String(message)), t);
 			}
 		}
 	}
@@ -170,30 +123,80 @@ public class TidbProtobufDeserializationSchema implements DeserializationSchema<
 		return resultTypeInfo;
 	}
 
-	public static Object getColumnValueString(Column column, ColumnInfo columnInfo) {
+	public static Object getColumnValue(Column column, ColumnInfo columnInfo) {
+		if (column.getIsNull()) {
+			return null;
+		}
 		String mysqlType = columnInfo.getMysqlType();
 		Object value;
-		if(mysqlType.equals("bigint")){
+		if (mysqlType.equals("bigint")) {
 			value = column.getInt64Value();
-		}else if(mysqlType.contains("int")){
+		} else if (mysqlType.contains("int")) {
 			value = Long.valueOf(column.getInt64Value()).intValue();
-		}else if(mysqlType.equals("double")){
+		} else if (mysqlType.equals("double")) {
 			value = column.getDoubleValue();
-		}else if(mysqlType.contains("blob")){
+		} else if (mysqlType.contains("blob")) {
 			value = column.getBytesValue().toByteArray();
-		}else {
+		} else {
 			value = StringData.fromString(column.getStringValue());
 		}
+
 		return value;
 	}
 
-	/**
-	 * Runtime converter that converts {@link JsonNode}s into objects of Flink Table & SQL
-	 * internal data structures.
-	 */
 	@FunctionalInterface
-	interface DeserializationRuntimeConverter extends Serializable {
-		Object convert(JsonNode jsonNode);
+	private interface DeserializationRuntimeConverter extends Serializable {
+		Object convert(Object object);
+	}
+
+
+	private Object convertField(
+		DeserializationRuntimeConverter fieldConverter,
+		Object value) {
+		return fieldConverter.convert(value);
+
+	}
+
+
+	private DeserializationRuntimeConverter createRowConverter(RowType rowType) {
+		final DeserializationRuntimeConverter[] fieldConverters = rowType.getFields().stream()
+			.map(RowType.RowField::getType)
+			.map(this::createConverter)
+			.toArray(DeserializationRuntimeConverter[]::new);
+		final String[] fieldNames = rowType.getFieldNames().toArray(new String[0]);
+
+		return value -> {
+			Tuple2<List<ColumnInfo>, List<Column>> columnsInfoAndValuesTuple = (Tuple2<List<ColumnInfo>, List<Column>>) value;
+			List<Column> columnList = columnsInfoAndValuesTuple.f1;
+			List<ColumnInfo> columnInfoList = columnsInfoAndValuesTuple.f0;
+			int arity = fieldNames.length;
+			GenericRowData row = new GenericRowData(arity);
+			for (int i = 0; i < arity; i++) {
+				Column column = columnList.get(i);
+				ColumnInfo columnInfo = columnInfoList.get(i);
+				Object columnValue = getColumnValue(column, columnInfo);
+				Object convertedField = convertField(fieldConverters[i], columnValue);
+				row.setField(i, convertedField);
+			}
+			return row;
+		};
+	}
+
+	private DeserializationRuntimeConverter wrapIntoNullableConverter(
+		DeserializationRuntimeConverter converter) {
+		return object -> {
+			if (object == null) {
+				return null;
+			}
+			try {
+				return converter.convert(object);
+			} catch (Throwable t) {
+				if (!ignoreParseErrors) {
+					throw t;
+				}
+				return null;
+			}
+		};
 	}
 
 	/**
@@ -207,66 +210,88 @@ public class TidbProtobufDeserializationSchema implements DeserializationSchema<
 	 * Creates a runtime converter which assuming input object is not null.
 	 */
 	private DeserializationRuntimeConverter createNotNullConverter(LogicalType type) {
-//		switch (type.getTypeRoot()) {
-//			case NULL:
-//				return jsonNode -> null;
-//			case BOOLEAN:
-//				return this::convertToBoolean;
-//			case TINYINT:
-//				return jsonNode -> Byte.parseByte(jsonNode.asText().trim());
-//			case SMALLINT:
-//				return jsonNode -> Short.parseShort(jsonNode.asText().trim());
-//			case INTEGER:
-//			case INTERVAL_YEAR_MONTH:
-//				return this::convertToInt;
-//			case BIGINT:
-//			case INTERVAL_DAY_TIME:
-//				return this::convertToLong;
-//			case DATE:
-//				return this::convertToDate;
-//			case TIME_WITHOUT_TIME_ZONE:
-//				return this::convertToTime;
-//			case TIMESTAMP_WITHOUT_TIME_ZONE:
-//				return this::convertToTimestamp;
-//			case FLOAT:
-//				return this::convertToFloat;
-//			case DOUBLE:
-//				return this::convertToDouble;
-//			case CHAR:
-//			case VARCHAR:
-//				return this::convertToString;
-//			case BINARY:
-//			case VARBINARY:
-//				return this::convertToBytes;
-//			case DECIMAL:
-//				return createDecimalConverter((DecimalType) type);
-//			case ARRAY:
-//				return createArrayConverter((ArrayType) type);
-//			case MAP:
-//			case MULTISET:
-//				return createMapConverter((MapType) type);
-//			case ROW:
-//				return createRowConverter((RowType) type);
-//			case RAW:
-//			default:
-//				throw new UnsupportedOperationException("Unsupported type: " + type);
-//		}
+		switch (type.getTypeRoot()) {
+			case NULL:
+				return value -> null;
+			case BOOLEAN:
+				return value -> Boolean.parseBoolean(value.toString());
+			case TINYINT:
+				return value -> Byte.parseByte(value.toString());
+			case SMALLINT:
+				return value -> Short.parseShort(value.toString());
+			case INTEGER:
+			case INTERVAL_YEAR_MONTH:
+				return value -> Integer.parseInt(value.toString());
+			case BIGINT:
+			case INTERVAL_DAY_TIME:
+				return value -> Long.parseLong(value.toString());
+			case DATE:
+				return this::convertToDate;
+			case TIME_WITHOUT_TIME_ZONE:
+				return this::convertToTime;
+			case TIMESTAMP_WITHOUT_TIME_ZONE:
+				return this::convertToTimestamp;
+			case FLOAT:
+				return value -> Float.parseFloat(value.toString());
+			case DOUBLE:
+				return value -> Double.parseDouble(value.toString());
+			case CHAR:
+			case VARCHAR:
+				return value -> StringData.fromString(value.toString());
+			case BINARY:
+			case VARBINARY:
+				return this::convertToBytes;
+			case DECIMAL:
+				return createDecimalConverter((DecimalType) type);
+			default:
+				throw new UnsupportedOperationException("Unsupported type: " + type);
+		}
 	}
 
-	private DeserializationRuntimeConverter wrapIntoNullableConverter(
-		DeserializationRuntimeConverter converter) {
-		return jsonNode -> {
-			if (jsonNode == null || jsonNode.isNull()) {
-				return null;
-			}
-			try {
-				return converter.convert(jsonNode);
-			} catch (Throwable t) {
-				if (!ignoreParseErrors) {
-					throw t;
-				}
-				return null;
-			}
+
+	private int convertToDate(Object value) {
+		LocalDate date = ISO_LOCAL_DATE.parse(value.toString()).query(TemporalQueries.localDate());
+		return (int) date.toEpochDay();
+	}
+
+	private int convertToTime(Object value) {
+		TemporalAccessor parsedTime = SQL_TIME_FORMAT.parse(value.toString());
+		LocalTime localTime = parsedTime.query(TemporalQueries.localTime());
+		// get number of milliseconds of the day
+		return localTime.toSecondOfDay() * 1000;
+	}
+
+	private TimestampData convertToTimestamp(Object value) {
+		TemporalAccessor parsedTimestamp;
+		switch (timestampFormat) {
+			case SQL:
+				parsedTimestamp = SQL_TIMESTAMP_FORMAT.parse(value.toString());
+				break;
+			case ISO_8601:
+				parsedTimestamp = ISO8601_TIMESTAMP_FORMAT.parse(value.toString());
+				break;
+			default:
+				throw new TableException(String.format("Unsupported timestamp format '%s'. Validator should have checked that.", timestampFormat));
+		}
+		LocalTime localTime = parsedTimestamp.query(TemporalQueries.localTime());
+		LocalDate localDate = parsedTimestamp.query(TemporalQueries.localDate());
+
+		return TimestampData.fromLocalDateTime(LocalDateTime.of(localDate, localTime));
+	}
+
+	private byte[] convertToBytes(Object value) {
+		byte[] bytes = value.toString().getBytes();
+		return bytes;
+	}
+
+	private DeserializationRuntimeConverter createDecimalConverter(DecimalType decimalType) {
+		final int precision = decimalType.getPrecision();
+		final int scale = decimalType.getScale();
+		return value -> {
+			BigDecimal bigDecimal = new BigDecimal(value.toString());
+			return DecimalData.fromBigDecimal(bigDecimal, precision, scale);
 		};
 	}
 }
+
+
